ecafed9a040d9de86c1050279ae1aa65a049b752
Caio Della Libera <caiodellalibera@id.uff.br>
2018-12-08 21:39:40 -0200

    validate de email; renomeei os form de cadastro; retirei os atributos de IES (ainda nao implementado) do index e do show de usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  validates :email, uniqueness: { case_sensitive: false}
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
