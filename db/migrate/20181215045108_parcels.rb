1e6e9bc8ac7f2671f6ead827a03aef018f9b2a03
matheusperrut <matheus_perrut@id.uff.br>
2018-12-15 04:52:18 +0000

    reorganizando migrations por conta de deploy no PG

--- db/migrate/20181215045108_parcels.rb
+++ db/migrate/20181215045108_parcels.rb
@@ -1,4 +1,4 @@
-class CreateParcels < ActiveRecord::Migration[5.2]
+class Parcels < ActiveRecord::Migration[5.2]
   def change
     create_table :parcels do |t|
       t.references :payment, foreign_key: true
