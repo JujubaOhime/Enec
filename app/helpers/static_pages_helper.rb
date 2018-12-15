1ede3726d5fa3d320a24928034d28e31f9ebc9f8
matheusperrut <matheus_perrut@id.uff.br>
2018-12-15 14:01:32 +0000

    Adicionando select com os pacotes

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+  def package_select
+    @packages = Package.all.includes(:lot)
+    
+    @packages.all.collect { |p| ["#{p.lot.name}, #{p.name},
+      #{number_to_currency(p.value, :unit => "R$ ", :separator => ",",
+      :delimiter => ".")}", p.id] }
+  end
 end
