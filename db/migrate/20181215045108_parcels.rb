774fb544b8ff32f3719cbf4bffae827cc9e915a4
matheusperrut <matheus_perrut@id.uff.br>
2018-12-19 23:15:57 +0000

    alterações nas migrations e reset do bd (hint: postgres não suporta change_column)

--- db/migrate/20181215045108_parcels.rb
+++ db/migrate/20181215045108_parcels.rb
@@ -3,7 +3,7 @@ class Parcels < ActiveRecord::Migration[5.2]
     create_table :parcels do |t|
       t.references :payment, foreign_key: true
       t.decimal :value
-      t.boolean :status
+      t.integer :status
 
       t.timestamps
     end
--- db/migrate/20181217142744_change_status_type_on_parcels.rb
+++
@@ -1,5 +0,0 @@
-class ChangeStatusTypeOnParcels < ActiveRecord::Migration[5.2]
-  def change
-    change_column :parcels, :status, :integer
-  end
-end
