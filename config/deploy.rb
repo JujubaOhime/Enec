d2b9788279739bd6849ee0fb78650354d090444f
matheusperrut <matheus_perrut@id.uff.br>
2018-12-20 01:53:35 +0000

    Adicionado config/application.yml ao linked files, para o Capistrano não apagar na hora do deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@gitlab.com:projetos_2018/enec2019.2.git"
 
 set :deploy_to, '/home/deploy/enec2019.2'
 
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 set :pty, true
